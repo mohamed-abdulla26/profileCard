c73706a27c9c83ecf5ec33ae07526ca8ab418299
mohamed abdulla <mohamedabdullasmy@gmail.com>
2025-01-29 17:01:54 +0530

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import App from './App'
 
 
-import { ProfilCard } from './ProfileCard.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
-    <ProfilCard />
+    <App />
+    
   </StrictMode>,
 )
